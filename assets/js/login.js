f3a5be8273fa4b0a84771e05dba17dadc189eebd
Anderson <andersonlopezmartinez@gmail.com>
2022-03-20 00:44:03 -0500

    Se agrega el proyecto

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,2 +1,10 @@
 $().dropdown('show')
+<<<<<<< HEAD
 hidden.bs.dropdown
+=======
+hidden.bs.dropdown
+
+$(document).ready(function() {
+    $("#login-form").validate();
+  });
+>>>>>>> fc29fef (Se agrega el proyecto)
